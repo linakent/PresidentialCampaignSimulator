a50bc91f37cf9507e19b36b8ef216ce055adf908
BT_2S14_18 <linak9@nycstudents.net>
2024-11-08 10:54:10 -0500

    Added code for deciding which party the user wants to be part of, with accompanying messages

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
         int votes;
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Welcome to Presidential Campaign Simulator.");
+        System.out.println("First, select a party from the following options: Democrat, Republican, or Independent.");
+        String party = s.nextLine();
+
+        System.out.println("Okay, you will be running as a " + party + ".");
+
+
+
+
     }
 }
