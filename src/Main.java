61691280ebf73bb038ed00693963434d876674f8
linakent <linak9@nycstudents.net>
2024-11-21 08:37:25 -0500

    Finished main procedure

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
         System.out.println("Welcome to Presidential Campaign Simulator.");
         System.out.println("First, select a party from the following options: Democrat, Republican, or Independent.");
         String party = s.nextLine();
-        System.out.println("Okay, you will be running as a " + party + ".");
 
         Candidate candidate;
         if (party.equalsIgnoreCase("Democrat")){
@@ -20,9 +19,24 @@ public class Main {
         else {
             candidate = new Independent();
     }
+        System.out.println("Okay, you will be running as a " + party + ".");
+        System.out.println("Let's start the campaign!");
 
+        votes +- candidate.campaign(s);
+        votes +- candidate.debate(s);
+        votes +- candidate.fundraising(s);
 
+        votes +- corruptionOption(s, votes);
 
+        System.out.println("Campaign over! Total votes: " + votes);
+        if (votes > 100) {
+            System.out.println("Congratulations, you won the election! Good luck with your presidency!");
+        }
+        else if (votes >= 0) {
+            System.out.println("Unfortunately, you did not win the election. Good luck next time.);
+        }
+        else {
+            System.out.println("You were disqualified for your shameless corruption.");
 
     }
 }
